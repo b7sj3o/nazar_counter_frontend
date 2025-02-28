5fb398bc183896d753a95d396a0f3869250b203d
Vitaliy Fedytnyk <vitalikfedytnyk@gmail.com>
2025-02-28 03:31:34 +0200

    v.1.2.1

--- src/pages/AnalyticsPage/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage/AnalyticsPage.tsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { getSales } from '../../services/api';
-import { ProductSale, SalesSummary } from '../../types/product';
+import { ProductSale } from '../../types/product';
 import "./AnalyticsPage.scss";
 import { useModalMessage } from '../../context/ModalMessageContext';
 import { ProductEditModal } from '../../components/ProductEditModal/ProductEditMdal';
 
 const AnalyticsPage: React.FC = () => {
     const [sales, setSales] = useState<ProductSale[]>([]);
-    const [salesSummary, setSalesSummary] = useState<SalesSummary>({ total_revenue: 0, total_amount: 0, total_earning: 0 });
     const [showAllSales, setShowAllSales] = useState(false);
     const { showModal } = useModalMessage();
     const [saleDetails, setSaleDetails] = useState<ProductSale | null>(null);
@@ -18,10 +17,8 @@ const AnalyticsPage: React.FC = () => {
             setSales(filteredSales);
         };
 
-
         fetchSales();
     }, []);
-
 
     const handleToggleShowAll = () => {
         setShowAllSales(!showAllSales);
@@ -32,39 +29,53 @@ const AnalyticsPage: React.FC = () => {
     }
 
     const renderSaleDetails = () => {
-
         if (!saleDetails) return null;
 
         return (
             <ProductEditModal
                 title='Деталі продажу'
                 onClose={() => setSaleDetails(null)}
-                >
+            >
                 <div className="sale-details">
                     <p>Назва: {saleDetails.product_name}</p>
                     <p>Виробник: {saleDetails.producer_name}</p>
                     <p>Тип продукту: {saleDetails.product_type}</p>
                     <p>Кількість проданого: {saleDetails.amount}</p>
                     <p>Ціна продажу: {saleDetails.sell_price} грн</p>
-                    <p>Дата: {new Date(saleDetails.date).toLocaleDateString('uk-UA')}</p>
+                    <p>Дата: {new Date(saleDetails.date).toLocaleDateString('uk-UA')}, {new Date(saleDetails.date).toLocaleTimeString('uk-UA', { hour: '2-digit', minute: '2-digit' })}</p>
                 </div>
             </ProductEditModal>
-            
         )
     }
+
+    const groupSalesByDate = (sales: ProductSale[]) => {
+        return sales.reduce((acc, sale) => {
+            const date = new Date(sale.date).toLocaleDateString('uk-UA');
+            if (!acc[date]) {
+                acc[date] = [];
+            }
+            acc[date].push(sale);
+            return acc;
+        }, {} as Record<string, ProductSale[]>);
+    };
+
+    const groupedSales = groupSalesByDate(sales);
+    console.log(groupedSales)
 
     return (
         <div className="sales-analytics-container">
             <h2>Аналітика Продажів</h2>
         
-            <div className="sales-list">
-                {(showAllSales ? sales : sales.slice(0, 10)).map((sale) => (
-                    <div key={sale.id} className="sale-item" onClick={() => setSaleDetails(sale)}>
-                        <h4>{sale.product_name} - {sale.producer_name} - {sale.product_type}</h4>
-                        <button className="sale-item-delete" onClick={(e) => {e.stopPropagation(); handleRemoveSale(sale.id)}}>Видалити</button>
-                        {/* <p>Кількість проданого: {sale.amount}</p>
-                        <p>Ціна продажу: {sale.sell_price} грн</p>
-                        <p>Дата: {new Date(sale.date).toLocaleDateString('uk-UA')}</p> */}
+            <div>
+                {Object.entries(groupedSales).map(([date, salesOnDate]) => (
+                    <div key={date} className="sales-list">
+                        <h3 className='sales-list-date'>{date}</h3>
+                        {(showAllSales ? salesOnDate : salesOnDate.slice(0, 10)).map((sale) => (
+                            <div key={sale.id} className="sale-item" onClick={() => setSaleDetails(sale)}>
+                                <h4>{sale.product_name} - {sale.producer_name} - {sale.product_type}</h4>
+                                <button className="sale-item-delete" onClick={(e) => {e.stopPropagation(); handleRemoveSale(sale.id)}}>Видалити</button>
+                            </div>
+                        ))}
                     </div>
                 ))}
             </div>
